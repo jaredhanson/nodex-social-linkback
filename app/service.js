e5a7d466be7f56cfd5241c2b17ff383e3edb526e
Jared Hanson <jaredhanson@gmail.com>
2018-07-02 17:47:06 -0700

    Add messaging services dependency.

--- app/service.js
+++ app/service.js
@@ -1,13 +1,16 @@
-exports = module.exports = function() {
+exports = module.exports = function(ms) {
   // Load modules.
   var Server = require('../lib/server');
   
+  console.log(process.env);
   
-  var server = new Server();
+  
+  var server = new Server(ms);
   return server;
 }
 
 exports['@singleton'] = true;
 exports['@implements'] = 'http://schemas.modulate.io/js/social/ILinkbackService';
 exports['@require'] = [
+  'http://i.bixbyjs.org/ms'
 ];
